d042aafa3be73c790402b06f8bdcdf885e864eed
KL_jiana <2436262516@qq.com>
2024-05-12 00:02:38 +0800

    update

--- kubejs/server_scripts/edit.js
+++ kubejs/server_scripts/edit.js
@@ -1,7 +1,5 @@
 // packmode: debug
 // 只要我想，随时写😋
 ItemEvents.rightClicked(event => {
-    const{enchantments}=event.item
-    event.server.tell(enchantments.size())
-    event.server.tell(enchantments.entrySet()[0].getKey())
+    //event.player.potionEffects.add("resistance",200,255)
 })
