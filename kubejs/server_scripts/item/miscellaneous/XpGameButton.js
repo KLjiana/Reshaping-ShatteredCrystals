b41398fafd93f7e8375231aea07256ccc1e3cd8c
KL_jiana <2436262516@qq.com>
2024-05-18 18:47:29 +0800

    update

--- kubejs/server_scripts/item/miscellaneous/XpGameButton.js
+++ kubejs/server_scripts/item/miscellaneous/XpGameButton.js
@@ -1,6 +1,7 @@
 let wearElytra = false
 let quality = "minecraft:iron_"
 const firearms = ["pointblank:glock17", '64x pointblank:ammo9mm', '64x pointblank:ammo9mm']
+const armor = ["boots", "leggings", "chestplate", "helmet"]
 ItemEvents.rightClicked(event => {
     const { player, item, server } = event
     const playerName = player.name.getString()
@@ -9,7 +10,9 @@ ItemEvents.rightClicked(event => {
         item.count--
         server.runCommandSilent(`/execute in lostcities:lostcity run teleport ${playerName} ~ 160 ~`)
         player.setChestArmorItem("minecraft:elytra")
-        //console.info(player.armorSlots)
+        for (let index = 0; index < 200; index++) {
+            console.info(index + ' || ' + player.inventory.getItem(index))
+        }
         event.server.schedule(100, () => {
             firearms.forEach(fire => player.inventory.add(fire))
             wearElytra = true
@@ -19,10 +22,9 @@ ItemEvents.rightClicked(event => {
 PlayerEvents.tick(event => {
     const { player } = event
     if (player.fallDistance == 0 && player.onGround && player.chestArmorItem == "minecraft:elytra" && wearElytra) {
-        player.setHeadArmorItem(quality + "helmet")
-        player.setChestArmorItem(quality + "chestplate")
-        player.setLegsArmorItem(quality + "leggings")
-        player.setFeetArmorItem(quality + "boots")
+        for (let index = 36; index <= 39; index++) {
+            player.inventory.setItem(index, quality + armor[index-36])
+        }
         wearElytra = false
     }
 })
