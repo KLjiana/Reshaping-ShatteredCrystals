1056a496420a78f4b63b9531b4055ca1523d98cc
KL_jiana <2436262516@qq.com>
2024-05-28 21:34:06 +0800

    update 我是傻逼

--- kubejs/server_scripts/item/miscellaneous/XpGameButton.js
+++ kubejs/server_scripts/item/miscellaneous/XpGameButton.js
@@ -3,14 +3,16 @@ let quality = "minecraft:iron_"
 const firearms = ["pointblank:m1911a1", "64x pointblank:ammo45acp", "64x pointblank:ammo45acp"]
 const armor = ["boots", "leggings", "chestplate", "helmet"]
 ItemEvents.rightClicked(event => {
+    console.info('touch')
     const { player, item, server } = event
     const playerName = player.name.getString()
 
-    if (item == "provefrom:xp_game_button") {
+    if (item == "newprovefrom:xp_game_button") {
         item.count--
         server.runCommandSilent(`/execute in lostcities:lostcity run teleport ${playerName} ~ 250 ~`)
         player.setChestArmorItem("minecraft:elytra")
         event.server.schedule(100, () => {
+            console.info('give you')
             firearms.forEach(fire => player.inventory.add(fire))
             wearElytra = true
         })
@@ -22,6 +24,7 @@ PlayerEvents.tick(event => {
         for (let index = 36; index <= 39; index++) {
             player.inventory.setItem(index, quality + armor[index - 36])
         }
+        console.info('clean!')
         wearElytra = false
     }
 })
